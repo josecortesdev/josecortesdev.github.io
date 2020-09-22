c68e940579ec0c3ec1b0427673f731cef8c5257d
José Cortés <61329277+josecortesdev@users.noreply.github.com>
2020-09-22 20:26:36 +0200

    Update p1fintool.js

--- p1fintool.js
+++ p1fintool.js
@@ -23,3 +23,32 @@ escribe.innerHTML= 'La cotización se ha modificado en un ' + resultado + ' %';
 
 
 });
+
+
+// --------------------- DESCUENTO
+
+//let precioproducto = document.getElementById('#precioproducto');
+//let porcentajeproducto = document.getElementById('#porcentajeproducto');
+
+let escribepreciofinal = document.querySelector('#escribepreciofinal');
+
+
+$('#descuentoform').submit(function (evento) {
+
+    evento.preventDefault(); 
+
+    
+
+    let preciop= $('#precioproducto').val();
+    let porcentajep= $('#porcentajeproducto').val();
+
+    preciop = parseFloat(preciop);
+    porcentajep = parseFloat(porcentajep);
+
+    let preciofinal = preciop - preciop * (porcentajep/100);   //preciop - preciop * (porcentajep/100)
+
+escribepreciofinal.innerHTML= 'Si descontamos el ' + porcentajep + '%,' + ' el precio final es: ' + preciofinal + ' euros.';
+
+
+
+});
